c7ec3e2d219bc86540c8f828009f853f7190d3ee
vahid133 <vetemadi87@gmail.com>
2017-09-16 10:06:31 +0430

    zone calss has been added

---
+++ src/Karim/Zone.java
@@ -0,0 +1,14 @@
+package Karim;
+
+public class Zone {
+	private int zId;
+	private String zName;
+	public double DZCoefficient;
+	public double BZCoefficient;
+	
+	public Zone(int id, String name){
+		this.zId=id;
+		this.zName=name;
+	}
+		
+}
