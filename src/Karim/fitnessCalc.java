f69bb8c53eec613e389833f0eb1de18be456c468
vahid133 <vetemadi87@gmail.com>
2017-09-20 02:06:51 +0430

    first

--- src/Karim/fitnessCalc.java
+++ src/Karim/fitnessCalc.java
@@ -9,9 +9,11 @@ public static double totalTime(Bug bug, Entry<Zone, Double> zone,
 		Developer developer) {
 	double startTime=0.0;
 	for(int j=0;j<bug.DB.length;j++){
-		if(bug.DB[j].endstartTime)
-			startTime=bug.DB[j];
+		if(bug.DB[j].endTime>startTime)
+			startTime=bug.DB[j].endTime;
 	}
+	
+	double 
 	return 0;
 }
 }
