8f0259eb2cb91cc8e2b208f084ae2c6378eba580
vahid133 <vetemadi87@gmail.com>
2017-09-23 20:20:21 +0330

    4

--- src/Karim/Test1.java
+++ src/Karim/Test1.java
@@ -1,37 +1,38 @@
 package Karim;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.nio.file.Files;
-import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.LineNumberReader;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Scanner;
+
 import org.moeaframework.Executor;
 import org.moeaframework.core.NondominatedPopulation;
 import org.moeaframework.core.Solution;
-
-import org.moeaframework.core.NondominatedPopulation;
 
 
 public class Test1 {
 
 	public static void main(String[] args) throws FileNotFoundException{
-		Initialization();
-		Assigning();
-		writeResult();
-		afterRoundPurning();
-		
-		
-		
+		int roundNum=5;
+		for(int i=1;i<=roundNum;i++){
+		Initialization(i);
+		NondominatedPopulation[] results = null; 
+		results=Assigning(results);
+		writeResult(i,results);
+		afterRoundUpdating();
+		}
 	}
 	
-	public static void Initialization() throws FileNotFoundException{
+	public static void Initialization( int roundNum) throws FileNotFoundException{
 		HashMap<String , Zone> columns=new HashMap<String, Zone>();
 		
 		//initialize developers
 		HashMap<Integer,Developer> developers=new HashMap<Integer,Developer>();
+		
 		Developer developer = null;
 		Scanner sc=new Scanner(System.in);
 		sc=new Scanner(new File(sc.nextLine()));
@@ -63,6 +64,7 @@ public class Test1 {
 			}
 			developers.put(developer.getID(), developer);
 		}
+		sc.close();
 		
 		//initialize bugs: consider dependent bugs
 		
@@ -70,33 +72,37 @@ public class Test1 {
 		HashMap<Integer,Bug> bugs=new HashMap<Integer,Bug>();
 		Bug bug=null;
 		sc=new Scanner(System.in);
-		sc=new Scanner(new File(sc.nextLine()));
-		while(sc.hasNextLine()){
-			if(i!=0){
+		for(File fileEntry:new File(sc.next()).listFiles() ){
+			sc=new Scanner(fileEntry);
+			LineNumberReader lnr=new LineNumberReader(new FileReader(fileEntry));
+			int numOfBugs=lnr.getLineNumber();
+			int n=numOfBugs*roundNum;
+			while(sc.hasNextLine() && n>=numOfBugs/5 && n<2*(numOfBugs/5) ){
+				n++;
+				if(i!=0){
+						for(String s:sc.next().split(" ")){
+							if(j!=0){
+							columns.put(Integer.toString(j),new Zone(j, s));
+							}
+							j++;
+						}	
+				}
+				else{
 					for(String s:sc.next().split(" ")){
 						if(j!=0){
-						columns.put(Integer.toString(j),new Zone(j, s));
+							bug.BZone_Coefficient.put(columns.get(s), Double.parseDouble(s));
+						}
+						else{
+							bug=new Bug(0);
+							bug.setID(Integer.parseInt(s));
 						}
 						j++;
 					}
-				
-			}
-			else{
-				for(String s:sc.next().split(" ")){
-					if(j!=0){
-						bug.BZone_Coefficient.put(columns.get(s), Double.parseDouble(s));
-					}
-					else{
-						bug=new Bug(0);
-						bug.setID(Integer.parseInt(s));
-					}
-					j++;
+					i++;
+					j=0;
 				}
-				i++;
-				j=0;
+				bugs.put(bug.getID(), bug);
 			}
-			bugs.put(bug.getID(), bug);
-		}
 		
 		/*set bug dependencies*/
 		Scanner sc1=new Scanner(System.in);
@@ -111,8 +117,11 @@ public class Test1 {
 				}
 				j++;
 			}
+			sc1.close();
 			
 		}
+		}
+		
 		
 		/* set competency dependencies*/
 		
@@ -127,28 +136,51 @@ public class Test1 {
 	}
 	
 	//assigning to developer
-	public static void Assigning(){
+	public static NondominatedPopulation[] Assigning(NondominatedPopulation[] results){
 		NondominatedPopulation result_Karim=new Executor().withProblemClass(CompetenceMulti2_problem.class).withAlgorithm("NSGAII")
 				.withMaxEvaluations(500).withProperty("populationSize",GA_Problem_Parameter.population)
 				.withProperty("sbx.rate", GA_Problem_Parameter.sbx_rate).withProperty("sbx.distributionIndex", GA_Problem_Parameter.sbx_distribution_index)
 				.withProperty("pm.rate", GA_Problem_Parameter.pm_rate).withProperty("pm.distributionIndex", GA_Problem_Parameter.pm_distribution_index)
 				.run();
-		
+		results[0]=result_Karim;
 		
 	    NondominatedPopulation result_me=new Executor().withProblemClass(InformationDifussion.class).withAlgorithm("NSGAII")
 				.withMaxEvaluations(500).withProperty("populationSize",GA_Problem_Parameter.population)
 				.withProperty("sbx.rate", GA_Problem_Parameter.sbx_rate).withProperty("sbx.distributionIndex", GA_Problem_Parameter.sbx_distribution_index)
 				.withProperty("pm.rate", GA_Problem_Parameter.pm_rate).withProperty("pm.distributionIndex", GA_Problem_Parameter.pm_distribution_index)
 				.run();
+	    results[1]=result_me;
 	    
+	    return results;
 	    
 	}
 	
-	public static void writeResult(){
+	public static void writeResult(int roundNum, NondominatedPopulation[] result) throws FileNotFoundException{
 		//write results to CSV for each round
+		PrintWriter pw=new PrintWriter(new File("solutions_Karim_round"+roundNum));
+		StringBuilder sb=new StringBuilder();
+		for(Solution solution:result[0]){
+			sb.append(solution.getObjective(0)+","+solution.getObjective(1));
+			sb.setLength(sb.length()-1);
+			sb.append("\n");
+		}
+		pw.write(sb.toString());
+		pw.close();
+		
+		pw=new PrintWriter(new File("solutions_Me_round"+roundNum));
+		sb.setLength(0);
+		for(Solution solution:result[1]){
+			sb.append(solution.getObjective(0)+","+solution.getObjective(1));
+			sb.setLength(sb.length()-1);
+			sb.append("\n");
+		}
+		pw.write(sb.toString());
+		pw.close();
+		
 	}
 	
-	public static void afterRoundPurning(){
-		//
+	public static void afterRoundUpdating(){
+		//update developers' zone
+		
 	}
 }
