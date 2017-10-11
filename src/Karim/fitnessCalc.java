b92a8b162904e21c4c81850bc29019f62b7b9495
vahid133 <vetemadi87@gmail.com>
2017-10-11 17:21:45 +0330

    from 18 mehr

--- src/Karim/fitnessCalc.java
+++ src/Karim/fitnessCalc.java
@@ -23,17 +23,20 @@ public static double totalTime(Bug bug, Entry<Zone, Double> zone,
 
 
  public static double getSimDev(Developer d1, Developer d2){
-	 double DDSim=0.0;
-	 for (Entry<Zone, Double>  zone:d1.DZone_Coefficient.entrySet())
-		 DDSim+=Math.abs(Double.parseDouble(zone.getValue().toString())+d2.getDZone_Coefficient().get(zone.getKey()));
-	 
-	 return DDSim;
+	 double DDSim_intersection=0.0;
+	 double DDSim_union=0.0;
+	 for (Entry<Zone, Double>  zone:d1.DZone_Coefficient.entrySet()){
+		 DDSim_intersection+=Math.min(Double.parseDouble(zone.getValue().toString()),d2.getDZone_Coefficient().get(zone.getKey()));
+		 DDSim_union+=Math.min(Double.parseDouble(zone.getValue().toString()),d2.getDZone_Coefficient().get(zone.getKey()));
+	 }
+		 
+	 return 1/(DDSim_intersection/DDSim_union);
  }
  
  public static double getSimBug(Developer d1,Bug b2, Zone z1){
 	 double DBSim=0.0;
 	 //for (Entry<Zone, Double>  zone:b2.BZone_Coefficient.entrySet())
-	 DBSim+=Math.abs(b2.BZone_Coefficient.get(z1))*d1.DZone_Coefficient.get(z1);
+	 DBSim+=Math.min(b2.BZone_Coefficient.get(z1),d1.DZone_Coefficient.get(z1));
 
 	 return DBSim;
  }
